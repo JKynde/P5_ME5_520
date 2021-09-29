3d7b0fe33e563c43bbd949a06d2e9ca50189f7d6
jonas <jonas@172.27.82.193>
2021-09-29 08:49:52 +0200

    bruh

--- PiezoMatricer.m
+++ PiezoMatricer.m
@@ -1,28 +1,31 @@
 clear 
 clc
 PiezoParameters;
-omega = 1000; %Vinkelhastighed
+j = sqrt(-1);
+f = 1*10^6; %frekvens
+omega = 2*pi*f; %Vinkelhastighed
 n=h_33*C_0; % Matrix parameter
 v_0 = sqrt(c33D/rho_P); % wave speed of compressional waves in the piezoelectric plate
+v_0b = sqrt(ElasticModolusBacking/rho_b);
 k=omega/v_0; % Wave number 
 Z0a = rho_P*v_0*S; % plane wave acoustic impedance of the piezoelectric plate.
-Zba = Z0a; % <- vides ikke endnu, men det er: Corresponding acoustic impedance of the backing Z_b^A Vi gætter foreløbigt på =rho_backing*v_0,backing*S
-TeA = [1/n n/1i*omega*C_0;-1i*omega*C_0 0];
-TaA = 1/(Zba-1i*Z0a*tan(k*d/2))*([Zba+1i*Z0a*cot(k*d) (Z0a)^2+1i*Z0a*Zba*cot(k*d)
-1 Zba-2i*Z0a*tan(k*d/2)]);
+Zba = rho_b*v_0b*S; % <- vides ikke endnu, men det er: Corresponding acoustic impedance of the backing Z_b^A Vi gætter foreløbigt på =rho_backing*v_0,backing*S Problemet er at der står den er afhængig af frekvens. Plus den er cone-shaped.
+TeA = [1/n n/j*omega*C_0;-j*omega*C_0 0];
+TaA = 1/(Zba-j*Z0a*tan(k*d/2))*([Zba+j*Z0a*cot(k*d) (Z0a)^2+j*Z0a*Zba*cot(k*d)
+1 Zba-2*j*Z0a*tan(k*d/2)]);
 TA = (TeA)*(TaA); % Totale transfer matrix
 
 % Ligningesystemet: TA*[F;v]=[V*I] skal løses.
 
 %%%%%%%%%%%%%%%%%%%%%%Nedenstående er fra impedans modellering artiklen.
-epsilon33S=C_0/(areal*d); 
+epsilon33S=C_0/(S*d); 
 K = sqrt(h_33*epsilon33S); %Static electromechanical coupling factor. Brugt til impedansen set fra tranducer terminalerne
 X_c0 = -1*j/(omega*C_0); % En eller anden modstand. Bruges til Z_electrical
-v_a = 1 ; % Wave speed of load TBD. 
+v_a = 1500 ; % Wave speed of load TBD. 
 k_a = omega/v_a ; % fuck mig
-l_a = 0.05; % Væskesøjle. Fuck mig.
+l_a = 0.1; % Væskesøjle. Fuck mig.
 Z_c = Z0a; % Characteristic impedance, antages at være ens med Z0a for nu.
-Z_a = 0; % Characteristic impedance of the load. Sættes til nul for nu. Skal nok udregnes for olien.
+Z_a = 1500*2000*S; % Characteristic impedance of the load. Sættes til nul for nu. Skal nok udregnes for olien.
 Z_m1 = X_c0*(K^2*(tan(k*d)/(k*d/2))*((Z_c*tan(k*d/2))/(Z_c*tan(k*d)+Z_a*tan(k_a*l_a))));
 Z_m2 = X_c0*(K^2*(tan(k*d)/(k*d/2))*((Z_a/2)*tan(k_a*l_a)/(Z_c*tan(k*d)+Z_a*tan(k_a*l_a))));
 Z_e = X_c0+Z_m1+Z_m2; % equivalent electrical impedance seen from transducer terminals.
--- PiezoParameters.m
+++ PiezoParameters.m
@@ -1,6 +1,6 @@
 % Priezoelektriske Materiale konstanter (PIC181).
-rho_P = 7.8; % Densitet i [g/cm^3]
-epsilon_0 = 8.854*10^-12; % permittivity of free space [F/m]
+rho_P = 7800; % Densitet i [g/cm^3]
+epsilon_0 = 8.854*10^(-12); % permittivity of free space [F/m]
 PermittivityPolarizationInDirection = 1200; % Relative permittivity in the polarization direction = epsilon_33^T/epsilon_0
 epsilon33T = PermittivityPolarizationInDirection*epsilon_0; %epsilon 33T skal bruges senere til at udregnes h_33, som vi skal bruge.
 PermittivityNormalDirection = 1500; % Relative permittivity in the norm direction epsilon_11^T/epsilon_0
@@ -14,13 +14,13 @@ k_33 = 66;
 k_15 = 0.63;
 
 % Piezoelectric Charge Coefficients [10^-12C/N)
-d_31 = -120;
-d_33 = 265;
-d_15 = 475;
+d_31 = -120*10^(-12);
+d_33 = 265*10^(-12);
+d_15 = 475*10^(-12);
 
 %Piezoelectric voltage Coefficients [10^-3Vm/N]
-g_31 = -11.2;
-g_33 = 25;
+g_31 = -11.2*10^(-3);
+g_33 = 25*10^(-3);
 
     % Acousto-mechenical properties
 %Frequency coefficients [Hz*m]
@@ -30,14 +30,13 @@ N_3 = 2010;
 N_t = 2110;
 
 %Elastic compliance coefficients [10^-12m^2/N]
-S11T = 11.8;
-S33E = 14.2;
+S11T = 11.8*10^(-12);
+S33E = 14.2*10^(-12);
 
-c33D = 16.6; % Elastic stiffness coefficient [10^10N/m^2]
+c33D = 16.6*10^10; % Elastic stiffness coefficient [10^10N/m^2]
 Q_m = 2000; % Mechanical quality factor
 
-C_0 = 0.104; % Kapacitans af pladen i nanofarad.
-areal = 1; % Areal, mangler tal
+C_0 = 0.104*10^(-9); % Kapacitans af pladen i nanofarad. Det er to af dem så det noget fuck endnu
 h_33=d_33/(S33E*epsilon33T); % Formel for h_33, som vi tror er Piezoelectric stiffness constant er regnet ud fra side 15 i https://link.springer.com/content/pdf/bbm%3A978-94-007-0579-1%2F1.pdf
 % Der er en antagelse at h_33 i Ultrasonic Nondestructive Evaluation
 % systems er den samme som i den artiklen hvor formlen for h_33 er fundet.
@@ -46,5 +45,9 @@ h_33=d_33/(S33E*epsilon33T); % Formel for h_33, som vi tror er Piezoelectric sti
 %modellerer det endnu)
 d = 0.002; %Plate thickness [m]
 S = (0.005/2)^2*pi; % Plate area
+
+%diverse
+ElasticModolusBacking = 2.1*10^11; %Backing materiale konstanter.[N/m^2]
+rho_b = 7800;%Backing densitet [kg/M^3]
 
 
