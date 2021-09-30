6c4e1335b345ca7f31948cd714324d5b4ad038e4
jonas <jonas@172.27.82.193>
2021-09-30 09:00:26 +0200

    Ændret navnet på Piezoparameters.m til Parameters.m

--- Parameters.m
+++ Parameters.m
@@ -1,3 +1,6 @@
+%Definitions
+j = sqrt(-1);
+
 % Priezoelektriske Materiale konstanter (PIC181).
 rho_P = 7800; % Densitet i [kg/m^3]
 epsilon_0 = 8.854*10^(-12); % permittivity of free space [F/m]
@@ -32,10 +35,8 @@ N_t = 2110;
 %Elastic compliance coefficients [10^-12m^2/N]
 S11T = 11.8*10^(-12);
 S33E = 14.2*10^(-12);
-
 c33D = 16.6*10^10; % Elastic stiffness coefficient [10^10N/m^2]
 Q_m = 2000; % Mechanical quality factor
-
 C_0 = 0.104*10^(-9); % Kapacitans af pladen i nanofarad. Det er to af dem så det noget fuck endnu
 h_33=d_33/(S33E*epsilon33T); % Formel for h_33, som vi tror er Piezoelectric stiffness constant er regnet ud fra side 15 i https://link.springer.com/content/pdf/bbm%3A978-94-007-0579-1%2F1.pdf
 % Der er en antagelse at h_33 i Ultrasonic Nondestructive Evaluation
@@ -43,10 +44,10 @@ h_33=d_33/(S33E*epsilon33T); % Formel for h_33, som vi tror er Piezoelectric sti
 
 %Geometri af en enkelt piezoplade(vi har to. Ved ikke hvordan vi lige
 %modellerer det endnu)
-d = 0.002; %Plate thickness [m]
-S = (0.005/2)^2*pi; % Plate area
+d_p = 0.002; %Plate thickness [m]
+Area = (0.005/2)^2*pi; % Plate area
 
-%diverse
+%Backing
 ElasticModolusBacking = 2.1*10^11; %Backing materiale konstanter.[N/m^2]
 rho_b = 7800;%Backing densitet [kg/M^3]m
 
@@ -54,4 +55,10 @@ rho_b = 7800;%Backing densitet [kg/M^3]m
 rho_oil = 800;
 v_sOil = 500;
 
-
+%Calculated parameters
+v_0 = sqrt(c33D/rho_P); % wave speed of compressional waves in the piezoelectric plate
+v_0b = sqrt(ElasticModolusBacking/rho_b); % Wave speed of compressional waves in the backing material
+n=h_33*C_0; % ElectricMatrix parameter.
+Z0a = rho_P*v_0*Area; % plane wave acoustic impedance of the piezoelectric plate.
+Zba = rho_b*v_0b*Area; % <- vides ikke endnu, men det er: Corresponding acoustic impedance of the backing Z_b^A Vi gætter foreløbigt på =rho_backing*v_0,backing*S Problemet er at der står den er afhængig af frekvens. Plus den er cone-shaped.
+ZrAa =v_sOil*rho_oil*Area; % Acoustic impedance of radiating medium. In this case the oil
