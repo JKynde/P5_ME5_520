82b3e04a976c7ef6a4e248066b66cf7847859953
jonas <jonas@172.27.82.193>
2021-09-30 12:26:37 +0200

    For at være helt ærligt så ved jeg ikke hvad jeg har ændret, men tør ikke revert local changes

--- Boderunner.m
+++ Boderunner.m
@@ -5,13 +5,14 @@ clear
 j = sqrt(-1);
 Parameters; % Først defineres alle parametre fra konstante Parameters.m
 V_in = 300*exp(j*deg2rad(0)); % Indgangs spændingsvisor.
+Tlmode = 1; % Tlmode til Matricer.m functionen. Der er om front layer matricen er ganget på. 1 for ja else ikke.
 
-f = logspace(5,6.5,500); % Lav en vektor logaritmisk fordelte indgange med n punkter.
+f = logspace(5,6.5,5000); % Lav en vektor logaritmisk fordelte indgange med n punkter.
 F_out = zeros(1,length(f)); % Lav en vektor med nuller lige så lang som vektor f.
 
 for n=1:length(f) % For loop som fylder F_out med resultaterne af modellen
     
-    F_out(n)=Matricer(f(n),V_in);
+    F_out(n)=Matricer(f(n),V_in,Tlmode);
    
 end
 
--- Matricer.m
+++ Matricer.m
@@ -1,4 +1,4 @@
-function [F,TA] = Matricer(f,V_in)
+function [F,TA] = Matricer(f,V_in,Tlmode)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 Parameters;
@@ -11,7 +11,11 @@ TaA = 1/(Zba-j*Z0a*tan(k*d_p/2))*([Zba+j*Z0a*cot(k*d_p) (Z0a)^2+j*Z0a*Zba*cot(k*
 1 Zba-2*j*Z0a*tan(k*d_p/2)]); % Acoustic piezo matrix.
 Tl = [cos(k_a*l_a) -j*Z0a_f*sin(k_a*l_a) % Acoustic front layer matrix
 -j*sin(k_a*l_a)/Z0a_f cos(k_a*l_a)];% Front layer matrice
+if Tlmode==1
 TA = (TeA)*(TaA)*Tl; % Total transfer matrix
+else 
+    TA=(TeA)*(TaA);% Total transfer matrix
+end
 SvIA = 1/(ZrAa*TA(2,1)+TA(2,2)); % v_t/I_in 
 ZinAe = (ZrAa*TA(1,1)+TA(1,2))/(ZrAa*TA(2,1)+TA(2,2)); % V_in/I_in
 S_VF = ZrAa*SvIA/ZinAe;   % F/V_in whoohoooo
