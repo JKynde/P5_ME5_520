ab69b0c1de300931d9ac624325a332e43df85d2e
jonas <jonas@172.27.82.193>
2021-10-11 15:09:49 +0200

    Hvis du lige kigger her så laver far lige et plot.

--- Pressurerunner.m
+++ Pressurerunner.m
@@ -3,21 +3,33 @@ clear
 Parameters;
 f = 10^6;
 omega = 2*pi*f;
-t=[0:1*10^-7:(pi/omega)+2*10^-7];
-z=[r_transducer*2:0.0001:0.02]; 
+t=[0:1*10^-8*2:2*10^-6];
+z=[r_transducer*2:0.0001:2*r_transducer+0.002]; 
 p_z=zeros(length(t),length(z));
+Wave = zeros(length(t),length(z));
 C=zeros(1,length(z));
 
 for i=1:length(t)
     
 for n=1:length(z)
-[p_z(i,n),C(n)]=Pressure(z(n),omega,t(i));
+[p_z(i,n),C(n),Wave(i,n)]=Pressure(z(n),omega,t(i));
 end
 Progress=(i/length(t))*100;
 Progress
 end
 
-plot(z,angle(p_z))
+
+
+for i=1:length(t)
+    hold on
+    plot(z,Wave(i,:))
+    pause(0.1)
+    
+end
+    
+    
+    
+    
 
 
 
