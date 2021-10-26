457117eac3286d0686c4910767aea2b8b4ed6339
Bruger <Bruger@172.27.94.238>
2021-10-26 13:34:47 +0200

    Tilføjet funktionalitet til boderunner. kan nu også outputte hastigheden, samt tilføjet forklaringer

--- Boderunner.m
+++ Boderunner.m
@@ -10,27 +10,28 @@ Tlmode = 1; % Tlmode til Matricer.m functionen. Der er om front layer matricen e
 f = logspace(5,6.5,5000); % Lav en vektor logaritmisk fordelte indgange med n punkter.
 F_out = zeros(1,length(f)); % Lav en vektor med nuller lige så lang som vektor f.
 Z_out = zeros(1,length(f)); 
+V_out = zeros(1,length(f));
 for n=1:length(f) % For loop som fylder F_out med resultaterne af modellen
-   
-    F_out(n)=Matricer(f(n),V_in,Tlmode);
-    [~,~,Z_out(n)]=Matricer(f(n),V_in,Tlmode);
+    %F_out(n)=Matricer(f(n),V_in,Tlmode);
+    [F_out(n),V_out(n),Z_out(n)]=Matricer(f(n),V_in,Tlmode); %for loopet løber over forskellige værdier af f og outputter tre vektorer som er F, Z, V
 end
 
-F_DB = 20*log10(abs(F_out));
+F_DB = 20*log10(abs(F_out)); % Her omregnes værdierne til db værdier
 Z_DB = 20*log10(abs(Z_out));
+V_DB = 20*log10(abs(V_out));
 figure;
 
-t=tiledlayout(2,1);
+t=tiledlayout(2,1); % Her plottes outputtet af matricer som bodeplot
 xlabel(t,'log spaced f values')
 
 nexttile
-semilogx(f,F_DB)
+semilogx(f,F_DB) % skriv enten F_DB, V_DB eller Z_DB
 grid on
 title('Magnitude plot of F(f)')
 ylabel('Magnitude of F(dB)')
 
 nexttile
-semilogx(f,rad2deg(angle(F_out)))
+semilogx(f,rad2deg(angle(F_out))) % Samme her
 grid on
 title('Angle plot of F(f)')
 ylabel('Angle of F(^\circ)')
