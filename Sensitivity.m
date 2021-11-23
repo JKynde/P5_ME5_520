5f13b0b8ffebb563ca86174169cdd219f809c469
jonas <jonas.kynde@live.dk>
2021-11-23 09:41:47 +0100

    Arbejdet videre på sensitivity

--- Sensitivity.m
+++ Sensitivity.m
@@ -1,9 +1,11 @@
+%% Initialisering
 clc
 clear
 % Det her scribt tjekker for input parameter sensitivitet 
 param=StructCreator();
 OGparam=param;
-fields = ["l_aa";"d_33";"l_a";"C_0"];
+Difftype = "Backward"; % Vælg diff type. Central, Forward eller Backward
+fields = ["l_aa";"d_33";"l_a";"C_0";"ElasticModolusAdhesive"];
 f = 10^6;
 V_in = 150;
 param.mode=3;
@@ -13,22 +15,40 @@ scalingfactor = 0.1;
 upscale = 1+scalingfactor;%
 downscale = 1-scalingfactor;%
 format long
+%% Parametervariation
 
 for n=1:length(fields)
-fprintf("%s is being varied. Its value is originally %e\n",fields(n),param.(fields(n)))
-
-param.(fields(n))=OGparam.(fields(n))*1.05;
-fprintf("Upping %s with a factor %f. Its value is now %e\n",fields(n),upscale,param.(fields(n)))
-Resultsup(n) = abs(Matricer2(f,V_in,param));
-param.(fields(n))=OGparam.(fields(n))*0.95;
-fprintf("Downing %s with a factor %f Its value is now %e \n",fields(n),downscale,param.(fields(n)))
-Resultsdown(n)=abs(Matricer2(f,V_in,param));
-param=OGparam;
+    fprintf("%s is being varied. Its value is originally %e\n",fields(n),param.(fields(n)))
+    param.(fields(n))=OGparam.(fields(n))*upscale;
+    fprintf("Upping %s with a factor %f. Its value is now %e\n",fields(n),upscale,param.(fields(n)))
+    fprintf("Evaluating resulting force output\n")
+    Resultsup(n) = abs(Matricer2(f,V_in,param));
+    param.(fields(n))=OGparam.(fields(n))*downscale;
+    fprintf("Downing %s with a factor %f Its value is now %e \n",fields(n),downscale,param.(fields(n)))
+    fprintf("Evaluating resulting force output\n")
+    Resultsdown(n)=abs(Matricer2(f,V_in,param));
+    stepsize(n)=abs( OGparam.(fields(n))-param.(fields(n)) );
+    param=OGparam;
 end
-
+%% Plotting og post processing
 for n=1:length(fields)
-    bruh(n) = (Resultsup(n)-Resultsdown(n))/BaseCase ;
+    Central(n) = abs( (Resultsup(n)-Resultsdown(n))/(2*stepsize(n)) * OGparam.(fields(n))/BaseCase ) ;
+    Forward(n) = abs( (Resultsup(n))/(stepsize(n)) * OGparam.(fields(n))/BaseCase ) ;
+    Backward(n) = abs( (Resultsdown(n))/(stepsize(n)) * OGparam.(fields(n))/BaseCase ) ;
 end
+if Difftype=="Central"
+    plot=Central;
+elseif Difftype=="Forward"
+    plot=Forward;
+elseif Difftype=="Backward"
+    plot=Backward;
+end
+plot=plot/max(plot);
 X = categorical(fields);
 X = reordercats(X,fields);
-bar(X,bruh)
+bar(X,plot)
+
+
+
+
+
