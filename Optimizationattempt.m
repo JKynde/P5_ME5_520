7460e55c49b562010e0eed4d36b816a86cbfb8f4
jonas <jonas.kynde@live.dk>
2021-12-14 10:20:27 +0100

    Dont do neggg added til optiattempt

--- Optimizationattempt.m
+++ Optimizationattempt.m
@@ -1,6 +1,10 @@
 clc
 clear
 OGparam=StructCreator(); % Originale parametre indlæses
+% OGparam.r_tranducer=0.00485/2;
+% OGparam.d_p=0.00205;
+% OGparam.ElasticModolusAdhesive=5.9*10^9;
+% OGparam.l_aa=0.025*10^-3;
 load("ExImpedance.mat");
 f_ex=Impedance(:,1);
 Z_ex=Impedance(:,2)./Impedance(:,3);
@@ -15,11 +19,13 @@ end
 OGparam.mode=3; % Indstil running mode for matricer 2
 OGparam.Tlmode=1; % Front lag eller eeeejjj.
 halvesmax=1000; % Maksimale antal halveringer af stepsize
-itermax=50;
-scaling = 0.5; % Initial scaling factor to determine initial stepsize for each parameter
+itermax=100; % Maksimale antal iterationer
+scaling = 0.3; % Initial scaling factor to determine initial stepsize for each parameter
 Bounds = 1; % Bounds?
-Boundscale = 2; % Scaleringsfaktor hvormed bounds defineres
-fields = ["l_aa";"l_a";"l_m";"ElasticModolusBacking";"rho_b";"rho_f";"ElasticModolusFront";"rho_a";"ElasticModolusAdhesive";"rho_m";"ElasticModolusMellem";"C_0"]; % Parametre som varieres
+dontgoneg = 1; % Prevent negative parameters
+
+Boundscale = 3; % Scaleringsfaktor hvormed bounds defineres
+fields = ["l_aa";"l_a";"l_m";"ElasticModolusBacking";"rho_b";"rho_f";"ElasticModolusFront";"rho_a";"ElasticModolusAdhesive";"rho_m";"ElasticModolusMellem";"C_0";"d_p";"rho_P";"d_33";"epsilon33T";"c33D";"h_33";"S33E"]; % Parametre som varieres
 % fields=fieldnames(OGparam);
 % fields=string(fields);
 % fields(1:2)=[]; %Fjerner param.mode og param.Tlmode fra variationen.
@@ -86,6 +92,11 @@ while stop==0
                 CurrentGuess.(fields(n))=Lowerbound(n);
             end
         end
+        if dontgoneg==1
+            if CurrentGuess.(fields(n))<0
+                CurrentGuess.(fields(n))=0;
+            end
+        end
         CurrentGuess=StructRecalculator(CurrentGuess); % Recalc
         if ZfitorF==1
             CurrentGuessResult=Zfit(f,V_in,CurrentGuess,Z_ex); % Evaluer
@@ -122,7 +133,7 @@ end
 bruh=1;
 for n=1:lengthfields
     if BestGuess.(fields(n))~=OGparam.(fields(n))
-        
+
         indexchangedvalues(bruh)=n;
         bruh=bruh+1;
         fprintf('A changed Value was %s, which originally was %e and now is %e\n',fields(n),OGparam.(fields(n)),BestGuess.(fields(n)))
@@ -154,7 +165,7 @@ if ZfitorF==1
     legend({'Measured impedance', 'New simulated impedance', 'Original simulated impedance'}, 'FontSize', 14)
     xlim([10^5 1.2*10^6]);
     xticks([10^5:10^5:1.2*10^6]);
-   
+
 
     f_b = logspace(5,6.5,5000);
     for n=1:length(f_b)
@@ -162,45 +173,45 @@ if ZfitorF==1
     end
     F_db = 20*log10(abs(F_sim));
     AngelFoutt = unwrap(rad2deg(angle(F_sim)));
-        figure;
+    figure;
 
-        t=tiledlayout(2,1); % Her plottes outputtet af matricer som bodeplot
-        xlabel(t,'log spaced f values')
+    t=tiledlayout(2,1); % Her plottes outputtet af matricer som bodeplot
+    xlabel(t,'log spaced f values')
 
-        nexttile
-        semilogx(f_b,F_db) % skriv enten F_DB, V_DB eller Z_DB
-        grid on
-        title('Magnitude plot of F(f)')
-        ylabel('Magnitude of F(dB)')
+    nexttile
+    semilogx(f_b,F_db) % skriv enten F_DB, V_DB eller Z_DB
+    grid on
+    title('Magnitude plot of F(f)')
+    ylabel('Magnitude of F(dB)')
 
-        nexttile
-        semilogx(f_b,AngelFoutt)
-        grid on
-        title('Angle plot of F(f)')
-        ylabel('Angle of F(^\circ)')
+    nexttile
+    semilogx(f_b,AngelFoutt)
+    grid on
+    title('Angle plot of F(f)')
+    ylabel('Angle of F(^\circ)')
 else
     OGresult=abs(Matricer2(f,V_in,OGparam));
 end
 fprintf('The original result was %f and the new result is %f\n',OGresult,BestGuessResult)
 
 for n=1:length(indexchangedvalues)
-changedfields(n)=fields(indexchangedvalues(n));
+    changedfields(n)=fields(indexchangedvalues(n));
 end
 
 figure
 X=categorical(changedfields);
 X=reordercats(X,changedfields);
 for n=1:length(changedfields)
-Y(1,n)=OGparam.(changedfields(n)) / OGparam.(changedfields(n));
-Y(2,n)=BestGuess.(changedfields(n))/OGparam.(changedfields(n));
+    Y(1,n)=OGparam.(changedfields(n)) / OGparam.(changedfields(n));
+    Y(2,n)=BestGuess.(changedfields(n))/OGparam.(changedfields(n));
 end
-bar(Y');
-set(gca,'XtickLabel',changedfields)
+bar(X,Y');
+%set(gca,'XtickLabel',changedfields)
 title('Original parameters compared to changed parameters', 'FontSize', 20)
 ylabel('Normalized parameter values', 'FontSize', 18)
 legend('Original parameter', 'changed parameter')
 
- 
+
 
 
 
